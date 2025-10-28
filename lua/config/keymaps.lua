eb49874203bef74a9cda39ef094d496f71a070ce
Martin Knoller Stocker <martin.knoller@brackalltron.ch>
2025-10-28 13:20:52 +0100

    update keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("i", "tn", "<Esc>", { desc = "escape the colmak way" })
+vim.keymap.set("n", "<leader>ww", ":w<Enter>", { desc = "write the current buffer" })
+vim.keymap.set("n", "<leader>bw", ":w<Enter>", { desc = "write the current buffer" })
